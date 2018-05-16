157f23dae336c9d61c77f0a6040515baa16e43b7
Tech-A42 <tech-a42@tech-a42s-air.home>
2018-05-16 08:02:00 +0100

    added comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  #Validates the emails
   validates :email, presence: true, 'valid_email_2/email': true
   validates :email, with: => /@["spartaglobal.com"]/
+
 
 
 end
